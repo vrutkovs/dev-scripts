171748a6cc92feecc2f70799dd05a729676d396c
Vadim Rutkovsky <vrutkovs@redhat.com>
2019-03-22 13:08:03 +0100

    Point the whole subdomain to the master

--- 06_deploy_bootstrap_vm.sh
+++ 06_deploy_bootstrap_vm.sh
@@ -27,7 +27,7 @@ done
 # NOTE: This is equivalent to the external API DNS record pointing the API to the API VIP
 IP=$(domain_net_ip ${INFRA_ID}-bootstrap baremetal)
 export API_VIP=$(dig +noall +answer "api.${CLUSTER_DOMAIN}" @$(network_ip baremetal) | awk '{print $NF}')
-echo "address=/api.${CLUSTER_DOMAIN}/${API_VIP}" | sudo tee /etc/NetworkManager/dnsmasq.d/openshift.conf
+echo "address=/.${CLUSTER_DOMAIN}/${API_VIP}" | sudo tee /etc/NetworkManager/dnsmasq.d/openshift.conf
 sudo systemctl reload NetworkManager
 
 # Wait for ssh to start
